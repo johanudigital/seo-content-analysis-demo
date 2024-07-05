2da9f7f0af68ca8ced81b73aa738dd6cb9cad293
johanudigital <johan@u-digital.nl>
2024-07-05 12:25:36 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import SEOContentOutlineTool from './seo-content-outline-tool';
 import './index.css';
 
+console.log('Starting application');
+
 ReactDOM.render(
   <Router>
     <SEOContentOutlineTool />
   </Router>,
   document.getElementById('root')
 );
+
+console.log('Rendered SEOContentOutlineTool');
